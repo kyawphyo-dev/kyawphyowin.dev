5d9113fe8864821a79c4bd0e8fab69d35fb8b65a
Kyaw Phyo Win <kyawphyowin242965@gmail.com>
2026-03-29 00:40:31 +0700

    fix: add base path for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { createRoot } from "react-dom/client";
 import Router from "./router";
 import { ThemeContextProvider } from "./contexts/ThemeContext";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <ThemeContextProvider>
-    <Router />
+    <BrowserRouter basename="/kyawphyowin.dev/">
+      <Router />
+    </BrowserRouter>
   </ThemeContextProvider>
 );
